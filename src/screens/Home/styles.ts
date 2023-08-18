b247dc4ca58aa87bd76b7d448e20a70f43792151
danielcreis <daniel.cvt7@gmail.com>
2023-08-18 17:47:28 -0300

    FirstApp

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#131016',
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingLeft: 20,
+    paddingRight: 20
+
+  },
+  eventName: {
+    color: '#FDFCFE', fontSize: 24, fontWeight: 'bold', marginTop: -50
+  },
+  input: {
+    height: 56,
+    backgroundColor: '#1F1E25',
+    marginTop: 30,
+    marginBottom: 30,
+    borderRadius: 5,
+    color: '#FDFCFE',
+    padding: 16,
+    fontSize: 16,
+    
+  }
+
+})
